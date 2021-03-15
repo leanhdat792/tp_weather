b09603990ce28866d90884926fbc4fe4f12e4720
leanhdat <le_anh_dat@allgrow-labo.jp>
2021-03-15 18:52:18 +0700

    Create Plugin Phan 6

--- tp-weather/includes/class.tp-weather-setting.php
+++ tp-weather/includes/class.tp-weather-setting.php
@@ -9,6 +9,7 @@ class TP_Weather_Setting {
 	protected $option;
 	protected $option_group = 'tp_weather_group';
 	public function __construct() {
+		$this->option = get_option('tp_weather_setting');
 		add_action('admin_menu', function() {
 			add_submenu_page(
 				'options-general.php',
@@ -24,5 +25,25 @@ class TP_Weather_Setting {
 	public function create_page() {
 		$option_group = $this->option_group;
 		require(TP_WEATHER_PLUGIN_DIR . 'views/tp-weather-setting.php');
+	}
+
+	public function register_setting() {
+		register_setting(
+			$this->option_group;
+			'tp_weather_setting',
+			[$this, 'save_setting']
+		);
+	}
+
+	public function save_setting($input){
+		$new_input = [];
+		if(isset($input['city_name']) && !empty($input['city_name'])) {
+			foreach ($input['city_name'] as $value) {
+				$new_input['city_name'] = urlencode(trim($value));
+			}
+		} else {
+			$new_input['city_name'][] = 'Ho+Chi+Minh';
+		}
+		return $new_input;
 	}
 }
